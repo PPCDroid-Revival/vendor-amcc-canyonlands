7d4df329b20ba49959a35d0bb7ef48e0c7a74055
Dmitry Eremin-Solenikov <dmitry_eremin@mentor.com>
2012-07-25 15:31:36 +0400

    Add hardware features
    Canyonlands support several Android hardware features (declared in
    canyonlands_hardware.xml). Some are emulated (bluetooth and camera) and
    should be substituded with real things (however xml files still are
    added to the rootfs).
    
    Change-Id: Ifb7b604865e1c00d7a7c36ce5ef2f82ead37ca84
    Signed-off-by: Dmitry Eremin-Solenikov <dmitry_eremin@mentor.com>

--- device.mk
+++ device.mk
@@ -11,3 +11,12 @@ PRODUCT_COPY_FILES := \
 
 PRODUCT_PACKAGES := \
 		    alsa.default \
+		    com.android.future.usb.accessory
+
+# Install the features available on this device.
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    device/amcc/canyonlands/canyonlands_hardware.xml:system/etc/permissions/canyonlands_hardware.xml \
+    device/amcc/canyonlands/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
+    device/amcc/canyonlands/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml
