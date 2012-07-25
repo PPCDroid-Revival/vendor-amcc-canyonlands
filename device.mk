178eb9b1e2ef78ac9f7d913c7c08fe1b7a55c824
Dmitry Eremin-Solenikov <dmitry_eremin@mentor.com>
2012-07-25 15:29:14 +0400

    Add support for loop-mounting sdcard
    Add support for loop-mounting SDCARD image. Put a VFAT-formatted sdcard.img
    file in the rootfs and it will be mounted and used instead of real SDcard/
    
    Change-Id: I39840a11d83fc1f963479f5e9f5ccec214fae12f
    Signed-off-by: Dmitry Eremin-Solenikov <dmitry_eremin@mentor.com>

--- device.mk
+++ device.mk
@@ -5,6 +5,7 @@ LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
 
 PRODUCT_COPY_FILES := \
+    device/amcc/canyonlands/vold.fstab:system/etc/vold.fstab \
     device/amcc/canyonlands/init.canyonlands.rc:root/init.canyonlands.rc \
     device/amcc/canyonlands/ueventd.canyonlands.rc:root/ueventd.canyonlands.rc
 
